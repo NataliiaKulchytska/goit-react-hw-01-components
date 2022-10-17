0ffddd90f2be04bc48b78e2f652d943636418f8e
NataliiaKulchytska <kylchitskaia@gmail.com>
2022-10-17 20:27:36 +0300

    profile

---
+++ src/components/App.jsx
@@ -0,0 +1,41 @@
+import { Profile } from './Profile/Profile';
+import { Statistics } from './Statistics/Statistics';
+import { Friends } from './FriendList/Friends';
+import { TransactionHistory } from './TransactionHistory/Transaction';
+
+import user from '../data/user.json';
+import data from '../data/data.json';
+import friends from '../data/friends.json';
+import transactions from '../data/transactions.json';
+
+export const App = () => {
+  return (
+    <div>
+        style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101'
+      }}
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+<Statistics title="Upload stats" stats={data} />
+      <Statistics stats={data} />
+      <Friends friends={friends} />
+      <TransactionHistory items={transactions} />
+
+    </div>
+    // <div
+    // 
+    // >
+    //   React homework template
+    // </div>
+  );
+};
